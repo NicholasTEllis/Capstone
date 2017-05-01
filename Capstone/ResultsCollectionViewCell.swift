828064b14da29261ffe7ee10007490edf46d69ef
NicholasTEllis <e.nicholas72@yahoo.com>
2017-05-01 11:01:28 -0400

    commented out some code, gonna prep for app store and add paging

---
+++ Capstone/ResultsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ResultsCollectionViewCell.swift
+//  Flix Finder
+//
+//  Created by Nicholas Ellis on 2/8/17.
+//  Copyright © 2017 Nicholas Ellis. All rights reserved.
+//
+
+import UIKit
+
+class ResultsCollectionViewCell: UICollectionViewCell {
+    
+}
